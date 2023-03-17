6306244367e7579806a37e987c02ca0a1343d9c5
Ercan Yılmaz <yilmazercan404@gmail.com>
2023-03-17 15:47:30 +0300

    Proje dosyası ekle.

---
+++ ulkeler oyunu/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace ulkeler_oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(55, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(240, 70);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(330, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(240, 70);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(55, 559);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(240, 70);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(330, 559);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(240, 70);
+            this.button4.TabIndex = 4;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1075, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(240, 70);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "BAŞLA";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(807, 218);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 38);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(803, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 38);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(56, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(486, 322);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 362);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 38);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1089, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 38);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "puan:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1178, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 38);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "00";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1385, 590);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(10, 10);
+            this.axWindowsMediaPlayer1.TabIndex = 11;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1178, 152);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 38);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Nirmala UI Semilight", 16.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1089, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 38);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "süre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1407, 751);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ ulkeler oyunu/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ulkeler_oyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+ private void renkler()
+        {
+            button1.BackColor = Color.White;
+            button2.BackColor = Color.White;
+            button3.BackColor = Color.White;
+            button4.BackColor = Color.White;
+        }
+        int sayaç = 0;
+        int puan = 0;
+        int süre = 50;
+
+        private void BtnAktiflik()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+             button5.Enabled = true;
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            SoundPlayer ses = new SoundPlayer();
+            ses.SoundLocation = "C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Properties\\Alone__Cv5bP2x1xG8__.wav";
+            ses.Play();
+            sayaç++;
+            label1.Visible = false;
+            button5.Text = "SONRAKİ";
+            label2.Text = " ";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+            if (sayaç == 1)
+            {
+
+                label3.Text = "Türkiye'ninbaşkenti neresidir";
+                pictureBox1.ImageLocation = @"C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\indir.jpeg";
+                button1.Text = "Antalya'nın başkenti neresidir";
+                button2.Text = "izmir";
+                button3.Text = "istanbul";
+                button4.Text = "Ankara";
+                label1.Text = "Ankara";
+                renkler();
+             
+            }
+          
+            if (sayaç == 2)
+            {
+                
+                pictureBox1.ImageLocation = @"C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\png-transparent-berlin-germany-berlin-reichstag-renaissance-gothic.png";
+                label3.Text = "Almanya'nın başkenti nresidir";
+                label1.Text = "Berlin";
+                button1.Text = "Dortmund";
+                button2.Text = "Berlin";
+                button3.Text = "Hamburg";
+                button4.Text = "Thüringen";
+                renkler();
+            }
+            if (sayaç == 3)
+            {
+                pictureBox1.ImageLocation = @"C:\Users\yilma\source\repos\ulkeler oyunu\ulkeler oyunu\Resources\indir (1).jpeg";
+                button1.Text = "Kalifornia";
+                button2.Text = "Texas";
+                button3.Text = "Washington";
+                button4.Text = "Havaii";
+                label3.Text = "Amerika'nın başkenti neresidir";
+                label1.Text = "Washington";
+                renkler();
+              
+            }
+            if (sayaç == 4)
+            {
+                pictureBox1.ImageLocation = @"C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\tokyo.png";
+                label3.Text = "Japonya'nın başkenti neresidir";
+                button1.Text = "Kyoto";
+                button2.Text = "Kamakura";
+                button3.Text = "Tokyo";
+                button4.Text = "Sapporo";
+                label1.Text = "Tokyo";
+                renkler();
+            }
+            if (sayaç == 5)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\lovepik-madrid-white-classical-architecture-png-image_401480644_wh1200.png";
+                label3.Text = "İspanya'nın başkemti neresidir ";
+                button1.Text = "Madrid";
+                button2.Text = "Barselona";
+                button3.Text = "Malaga";
+                button4.Text = "Kurtuba";
+                label1.Text = "Madrid";
+                renkler();
+            }
+            if (sayaç == 6)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\1661954496-Untitleddesign-2022-08-31T180053459.png";
+                label3.Text = "Suudi Arabistan'ın başkenti neresidir";
+                button1.Text = "Mekke";
+                button2.Text = "Medine";
+                button3.Text = "Cidde";
+                button4.Text = "Riyad";
+                label1.Text = "Riyad";
+                renkler();
+            }
+            if (sayaç == 7)
+            {
+                pictureBox1.ImageLocation = "C:\\Users\\yilma\\source\\repos\\ulkeler oyunu\\ulkeler oyunu\\Resources\\ancient-rome-landscape-ruins-png-favpng-zLFyzAfRgr9Vfytd5PTC13Jvk.jpg";
+                label3.Text = "İtalya'nın başkenti neresidir";
+                button1.Text = "Venedik";
+                button2.Text = "Roma";
+                button3.Text = "Milano";
+                button4.Text = "Floransa";
+                label1.Text = "Roma";
+                renkler();
+            }
+            if (sayaç == 8)
+            {
+                label3.Text = "Mısır'ın başlenti neresidir";
+                button1.Text = "Kahire";
+                button2.Text = "Dimyat";
+                button3.Text = "Süveyş";
+                button4.Text = "İskenderiye";
+                label1.Text = "Kahire";
+                renkler();
+                button5.Enabled = true;
+            }
+            if (sayaç == 9)
+            {
+                
+                int a = puan + Convert.ToInt32(süre);
+                label5.Text = a.ToString();
+                timer1.Stop();
+                MessageBox.Show("TEBRİKLER OYUN BİTTİ\n" +"PUANINIZ= "+ a);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = button1.Text;
+            if (label1.Text == button1.Text)
+            {
+                button1.BackColor = Color.Green;
+                puan += 10;
+                label5.Text = puan.ToString();
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+                puan -= 5;
+                label5.Text = puan.ToString();
+            }
+            BtnAktiflik();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = button2.Text;
+            if (label1.Text == button2.Text)
+            {
+                button2.BackColor = Color.Green;
+                puan += 10;
+                label5.Text = puan.ToString();
+            }
+            else
+            {
+                button2.BackColor = Color.Red;
+                puan -= 5;
+                label5.Text = puan.ToString();
+            }
+            BtnAktiflik();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Text = button3.Text;
+            if (label1.Text == button3.Text)
+            {
+                button3.BackColor = Color.Green;
+                puan += 10;
+                label5.Text = puan.ToString();
+                
+            }
+            else
+            {
+                button3.BackColor = Color.Red;
+                puan -= 5;
+                label5.Text = puan.ToString();
+            }
+            BtnAktiflik();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label2.Text = button4.Text;
+
+            if (label1.Text == button4.Text)
+            {
+                button4.BackColor = Color.Green;
+                puan += 10;
+                label5.Text = puan.ToString();
+            }
+            else
+            {
+                button4.BackColor = Color.Red;
+                puan -= 5;
+                label5.Text = puan.ToString();
+            }
+            BtnAktiflik();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            süre--;
+            label6.Text = süre.ToString();
+            if (süre == 0)
+            {
+                timer1.Stop();
+            }
+        }
+    }
+}
